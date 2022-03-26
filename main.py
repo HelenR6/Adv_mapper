048643b069e6785e2445ef79ff992f2e74162cf7
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2022-03-26 07:32:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from torchvision import transforms
 import h5py
 import json
 import numpy as np
+from advertorch.attacks import LinfPGDAttack, L2PGDAttack,L1PGDAttack
 from base import CustomResNet
+import torch.nn as nn
+
 preprocess = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(256),
@@ -29,3 +32,18 @@ preprocess = transforms.Compose([
             std=[0.229, 0.224, 0.225])
             ])
 mapper=RegMapper(arch='st_resnet',session='s_stretch_session1',preprocess=preprocess)
+adversary = L2PGDAttack(
+mapper, loss_fn=nn.MSELoss(reduction="sum"), eps=14.2737,
+nb_iter=20, eps_iter=1.784, rand_init=True, clip_min=-2.1179, clip_max=2.6400,
+targeted=False)
+for i, (images, target) in enumerate(data_loader):
+    # print(images.shape)
+    print("target shape")
+    print(target.shape)
+    # mapper.forward(images.double())
+    adv_untargeted = adversary.perturb(images.double(), target.double())
+    # mapper.forward(adv_untargeted.double())
+    diff=adv_untargeted-images
+    print(diff.shape)
+    diff_norm=torch.norm(diff,p=2,dim=(1,2,3))
+    print(diff_norm)
