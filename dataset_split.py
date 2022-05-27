bc0726cc98c5ea34758c3f5c8dc6d228345ebee6
Yifei_Ren <59241451+HelenR6@users.noreply.github.com>
2022-05-27 17:20:24 -0400

    Update dataset_split.py

--- dataset_split.py
+++ dataset_split.py
@@ -8,19 +8,23 @@ from torchvision.io import read_image
 from torch.utils.data import Dataset
 from sklearn.model_selection import KFold
 class CustomImageDataset(Dataset):
-    def __init__(self, session=None,transform=None,mode=None):
+    def __init__(self, session=None,transform=None,mode=None,input_path=None,index=None):
         self.transform = transform
+        self.session=session
+        self.input_path=input_path
+        self.index=index
+        self.mode=mode
         session_path=session.replace('_','/')
         final_path=session_path[:-1]+'_'+session_path[-1:]
-        f = h5py.File('/content/gdrive/MyDrive/npc_v4_data.h5','r')
+        f = h5py.File(f'{input_path}/npc_v4_data.h5','r')
         temp=np.empty([640,1])
         kfold = KFold(n_splits=5, shuffle=True,random_state=42)
         train_ids=[]
         test_ids=[]
-        for fold,(train_ids,test_ids) in enumerate(kfold.split(temp)):
-            if fold==0:
-                train_ids=train_ids
-                test_ids=test_ids
+        for fold,(train_idx,test_idx) in enumerate(kfold.split(temp)):
+            if fold==index:
+                train_ids=train_idx
+                test_ids=test_idx
         if mode=='train':
           image_data = f['images/naturalistic'][train_ids]
         if mode=='val_id':
@@ -33,7 +37,7 @@ class CustomImageDataset(Dataset):
           n1 = f.get('neural/naturalistic/monkey_'+final_path)[:]
           self.target=np.mean(n1, axis=0)[train_ids]
         if mode=='val_id':
-          n1 = f.get('neural/naturalistic/monkey_'+final_path)[:]
+          n1 = f.get('neural/naturalistic/monkey_'+final_path)
           self.target=np.mean(n1, axis=0)[test_ids]
         if mode=='val_ood':
           n1 = f.get('neural/synthetic/monkey_'+final_path)[:]
@@ -41,6 +45,26 @@ class CustomImageDataset(Dataset):
         
     def __len__(self):
         return self.images.shape[0]
+    def get_param(self):
+      session_path=self.session.replace('_','/')
+      final_path=session_path[:-1]+'_'+session_path[-1:]
+      f = h5py.File(f'{self.input_path}/npc_v4_data.h5','r')
+      temp=np.empty([640,1])
+      kfold = KFold(n_splits=5, shuffle=True,random_state=42)
+      train_ids=[]
+      test_ids=[]
+      for fold,(train_idx,test_idx) in enumerate(kfold.split(temp)):
+        if fold==self.index:
+            train_ids=train_idx
+            test_ids=test_idx
+      if self.mode=='train':
+        return train_ids,self.target.shape
+      if self.mode=='val_id':
+        return test_ids,self.target.shape
+      if self.mode=='val_ood':
+        return list(range(0, self.target.shape[1])),self.target.shape
+
+
 
     def __getitem__(self, idx):
         image = self.images[idx,:]
